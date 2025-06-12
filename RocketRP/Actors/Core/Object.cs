Drogebot/RocketRP

commit 77ac7313b0ad826d9b7f3c9997ae36838623b335

ObjectTarget defines Target Type

RocketRP/Actors/Core/Object.cs
@@ -103,10 +103,11 @@ namespace RocketRP.Actors.Core
 				//else propertyInfo.SetValue(obj, Enum.Parse(propertyType, "".Deserialize(br)));							// Remove the 2 lines below and uncomment these 2 lines
 				return Enum.Parse(propertyType, br.ReadString());
 			}
-			else if (propertyType == typeof(ObjectTarget))
+			else if (propertyType.GetInterface("IObjectTarget") == typeof(IObjectTarget))
 			{
 				if (type != "ObjectProperty") throw new InvalidDataException($"Expected type ObjectProperty for {obj.GetType().FullName}.{propName} but got {type}");
-				return ObjectTarget.Deserialize(br);
+				var objectTarget = propertyType.GetMethod("Deserialize", new Type[] { typeof(BinaryReader) }).Invoke(null, new object[] { br });
+				return objectTarget;
 			}
 			else if (propertyType.GetInterface("IArrayProperty") == typeof(IArrayProperty))
 			{
@@ -252,7 +253,7 @@ namespace RocketRP.Actors.Core
 				bw.Write(value.GetType().Name);
 				bw.Write(enumvalue.ToString());
 			}
-			else if (value is ObjectTarget objectvalue)
+			else if (value is ObjectTarget<ClassObject> objectvalue)
 			{
 				bw.Write("ObjectProperty");
 				bw.Write(4);

RocketRP/ObjectTarget.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RocketRP.Actors.Core;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,14 +8,26 @@ using System.Threading.Tasks;
 
 namespace RocketRP
 {
-    public struct ObjectTarget
+    public struct ObjectTarget<TObject> : IObjectTarget where TObject : Actors.Core.Object
     {
         public bool IsActor { get; set; }
         public int TargetIndex { get; set; }
 
-        public static ObjectTarget Deserialize(BinaryReader br)
-        {
-            return new ObjectTarget
+		public TObject? GetObject(List<Actors.Core.Object> objects)
+		{
+			if (objects[TargetIndex] is TObject obj) return obj;
+			else Console.WriteLine("ObjectTarget references wrong object type!");
+			return null;
+		}
+
+		public ClassObject? GetObject(List<string> objectTypes)
+		{
+            return new ClassObject(objectTypes[TargetIndex]);
+		}
+
+		public static ObjectTarget<TObject> Deserialize(BinaryReader br)
+		{
+			return new ObjectTarget<TObject>
             {
                 IsActor = false,
                 TargetIndex = br.ReadInt32(),
@@ -26,20 +39,38 @@ namespace RocketRP
             bw.Write(TargetIndex);
         }
 
-        public static ObjectTarget Deserialize(BitReader br)
+        public TObject? GetObject(Dictionary<int, ActorUpdate> actorUpdates, List<string> objectTypes)
         {
-            var objectTarget = new ObjectTarget();
+            if (TargetIndex == 0) return null;
+            if (IsActor)
+            {
+                if (actorUpdates[TargetIndex].Actor is TObject obj) return obj;
+                else Console.WriteLine("ObjectTarget references wrong actor type!");
+            }
+            else
+            {
+                if (new ClassObject(objectTypes[TargetIndex]) is TObject obj) return obj;
+                else Console.WriteLine("ObjectTarget references wrong object type!");
+            }
 
-            objectTarget.IsActor = br.ReadBit();
-            objectTarget.TargetIndex = br.ReadInt32();
+            return null;
+        }
 
-            return objectTarget;
+        public static ObjectTarget<TObject> Deserialize(BitReader br)
+        {
+            return new ObjectTarget<TObject>
+            {
+                IsActor = br.ReadBit(),
+                TargetIndex = br.ReadInt32(),
+            };
         }
 
         public void Serialize(BitWriter bw)
 		{
 			bw.Write(IsActor);
 			bw.Write(TargetIndex);
-		}
-	}
+        }
+    }
+
+    public interface IObjectTarget { }
 }

RocketRP.TrainingCLI/Program.cs
@@ -79,6 +79,11 @@ static void ParseTraining(string trainingPath, string outputPath, bool enforceCR
 			//if (File.Exists(outputFilePath)) return;
 			Console.WriteLine($"Parsing training: {trainingPath}...");
 			var training = SaveData<SaveData_GameEditor_Training_TA>.Deserialize(trainingPath, true, enforceCRC);
+			var trainingData = training.Properties.TrainingData?.GetObject(training.Objects);
+			if(trainingData is not null)
+			{
+				Console.WriteLine($"Training Name: {trainingData.TM_Name}");
+			}
 
 			Console.WriteLine($"Converting to JSON...");
 			var jsonData = serializer.Serialize(training, prettyPrint);

RocketRP/ActorUpdate.cs
@@ -1,4 +1,5 @@
-﻿using RocketRP.Actors.Engine;
+﻿using RocketRP.Actors.Core;
+using RocketRP.Actors.Engine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace RocketRP
 		public ChannelState State { get; set; }
 		public int? NameId { get; set; }
 		public string? Name { get; set; }
-		public ObjectTarget TypeId { get; set; }
+		public ObjectTarget<ClassObject> TypeId { get; set; }
 		public string TypeName { get; set; }
 		public int ObjectId { get; set; }
 		public string ObjectName { get; set; }
@@ -45,7 +46,7 @@ namespace RocketRP
 						actorUpdate.Name = replay.Names[actorUpdate.NameId.Value];
 					}
 
-					actorUpdate.TypeId = ObjectTarget.Deserialize(br);
+					actorUpdate.TypeId = ObjectTarget<ClassObject>.Deserialize(br);
 					if (actorUpdate.TypeId.IsActor)
 					{
 						Console.WriteLine("Warning: New Actor referenced existing Actor as type?");

RocketRP/Actors/Core/ClassObject.cs
@@ -1,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketRP.Actors.Core
+{
+	public class ClassObject : Core.Object
+	{
+		public string Name { get; set; }
+
+		public ClassObject(string name)
+		{
+			Name = name;
+		}
+	}
+}

RocketRP/Actors/Engine/Actor.cs
@@ -1,5 +1,4 @@
-﻿using RocketRP.Actors.Core;
-using RocketRP.DataTypes;
+﻿using RocketRP.DataTypes;
 using RocketRP.DataTypes.Enums;
 using System;
 using System.Collections.Generic;
@@ -22,7 +21,7 @@ namespace RocketRP.Actors.Engine
 		public Rotator RelativeRotation { get; set; }
 		public Vector RelativeLocation { get; set; }
 		public Vector Velocity { get; set; }
-		public ObjectTarget Instigator { get; set; }
+		public ObjectTarget<Pawn> Instigator { get; set; }
 		public bool bNetOwner { get; set; }
 		public bool bBlockActors { get; set; }
 		public bool bCollideWorld { get; set; }
@@ -30,8 +29,8 @@ namespace RocketRP.Actors.Engine
 		public bool bHardAttach { get; set; }
 		public bool bTearOff { get; set; }
 		public bool bHidden { get; set; }
-		public ObjectTarget Base { get; set; }
-		public ObjectTarget Owner { get; set; }
+		public ObjectTarget<Actor> Base { get; set; }
+		public ObjectTarget<Actor> Owner { get; set; }
 		public ECollisionType ReplicatedCollisionType { get; set; }
 		public ENetRole Role { get; set; }
 		public ENetRole RemoteRole { get; set; }

RocketRP/Actors/Engine/GameReplicationInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RocketRP.Actors.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace RocketRP.Actors.Engine
 {
 	public class GameReplicationInfo : ReplicationInfo
 	{
-		public ObjectTarget Winner { get; set; }
+		public ObjectTarget<Actor> Winner { get; set; }
 		public string ServerName { get; set; }
 		public int TimeLimit { get; set; }
 		public int GoalScore { get; set; }
@@ -18,6 +19,6 @@ namespace RocketRP.Actors.Engine
 		public bool bMatchIsOver { get; set; }
 		public bool bMatchHasBegun { get; set; }
 		public bool bStopCountDown { get; set; }
-		public ObjectTarget GameClass { get; set; }
+		public ObjectTarget<ClassObject> GameClass { get; set; }
 	}
 }

RocketRP/Actors/Engine/Pawn.cs
@@ -12,7 +12,7 @@ namespace RocketRP.Actors.Engine
 		public Vector RootMotionInterpCurveLastValue { get; set; }
 		public float RootMotionInterpCurrentTime { get; set; }
 		public float RootMotionInterpRate { get; set; }
-		public ObjectTarget PlayerReplicationInfo { get; set; }
+		public ObjectTarget<PlayerReplicationInfo> PlayerReplicationInfo { get; set; }
 		public float AirControl { get; set; }
 		public float JumpZ { get; set; }
 		public float AccelRate { get; set; }
@@ -26,7 +26,7 @@ namespace RocketRP.Actors.Engine
 		public bool bSimulateGravity { get; set; }
 		public bool bIsCrouched { get; set; }
 		public bool bIsWalking { get; set; }
-		public ObjectTarget Controller { get; set; }
+		public ObjectTarget<Actor> Controller { get; set; }	// Is of Controller type, but those don't appear in replays
 
 
 		// These are old properties that were removed

commit 5c1261a434e82b2010304dc86ab1f0ef6bf32b80

Fixed Training Parser
Fixed bug introduced in 77ac731

RocketRP/Actors/Core/Object.cs
@@ -153,7 +153,7 @@ namespace RocketRP.Actors.Core
 			else if (propertyType.GetInterface("IObjectTarget") == typeof(IObjectTarget))
 			{
 				if (type != OBJECT_PROPERTY && type != ARRAY_PROPERTY) throw new InvalidDataException($"Expected type {OBJECT_PROPERTY} for {propertyType.Name} but got {type}");
-				return ObjectTarget<ClassObject>.Deserialize(br);
+				return propertyType.GetMethod(nameof(ObjectTarget<ClassObject>.Deserialize), BindingFlags.Public | BindingFlags.Static, [typeof(BinaryReader)])!.Invoke(null, [br])!;
 			}
 			else
 			{
@@ -320,7 +320,7 @@ namespace RocketRP.Actors.Core
 				bw.Write(enumvalue.ToString());
 				return;
 			}
-			else if (value is ObjectTarget<ClassObject> objectvalue)
+			else if (value is IObjectTarget objectvalue)
 			{
 				type = OBJECT_PROPERTY;
 				objectvalue.Serialize(bw);

RocketRP/ObjectTarget.cs
@@ -56,7 +56,7 @@ namespace RocketRP
             return null;
         }
 
-        public static ObjectTarget<TObject> Deserialize(BitReader br)
+		public static ObjectTarget<TObject> Deserialize(BitReader br)
         {
             return new ObjectTarget<TObject>
             {
@@ -72,5 +72,8 @@ namespace RocketRP
         }
     }
 
-    public interface IObjectTarget { }
+    public interface IObjectTarget
+    {
+        public void Serialize(BinaryWriter bw);
+    }
 }